8712789a12046b5c5a1e870c97dfa263ae1c9d91
pkj-ind <pramodix@gmail.com>
2020-08-11 02:23:46 +0530

    code formatting using pretier

--- components/MealList.js
+++ components/MealList.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
-import MealItem from "./MealItem"
+import MealItem from "./MealItem";
 const MealList = (props) => {
-
-    const renderMealItem = (itemData) => {
-        // console.log("Item Title", itemData.item.title);
-        return (
-          <MealItem
-            title={itemData.item.title}
-            image={itemData.item.imageUrl}
-            onSelectMeal={() => {
-              props.navigation.navigate({
-                routeName: "Meal Details",
-                params: {
-                  mealId: itemData.item.id,
-                },
-              });
-            }}
-            duration={itemData.item.duration}
-            complexity={itemData.item.complexity}
-            affordability={itemData.item.affordability}
-          />
-        );
-      };
-
+  const renderMealItem = (itemData) => {
+    // console.log("Item Title", itemData.item.title);
     return (
-        <View style={styles.screen}>
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "Meal Details",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.screen}>
       <FlatList
         data={props.listData}
         keyExtractor={(item, index) => item.id}
@@ -34,16 +33,16 @@ const MealList = (props) => {
         style={{ width: "100%" }}
       />
     </View>
-    )
-}
+  );
+};
 
-export default MealList
+export default MealList;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 15,
-      },
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+  },
+});
