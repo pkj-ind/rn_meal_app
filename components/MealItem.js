3e25c6ac7662fea9f5c747b95b0aa423b8539b5c
pkj-ind <pramodix@gmail.com>
2020-08-12 14:56:45 +0530

    created defaultText comp for setting fonts

--- components/MealItem.js
+++ components/MealItem.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   ImageBackground,
 } from "react-native";
+import DefaultText from "../components/DefaultText"
 
 const MealItem = (props) => {
   return (
@@ -22,9 +23,9 @@ const MealItem = (props) => {
       </View>
 
       <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-        <Text>{props.duration}m</Text>
-        <Text>{props.complexity.toUpperCase()}</Text>
-        <Text>{props.affordability.toUpperCase()}</Text>
+        <DefaultText>{props.duration}m</DefaultText>
+        <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
       </View>
     </TouchableOpacity>
     </View>
