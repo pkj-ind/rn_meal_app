e9dbb9ad46f55e687a3c43bee276e808cf133592
pkj-ind <pramodix@gmail.com>
2020-08-19 03:02:30 +0530

    Fallback message for mealcategory screen

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import {View, StyleSheet} from "react-native"
 import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import DefaultText from "../components/DefaultText"
 
 const CategoryMealScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
@@ -11,7 +12,13 @@ const CategoryMealScreen = (props) => {
   const displayedMeals = avblMeals.filter((meal) => {
     return meal.categoryIds.indexOf(catId) >= 0;
   });
-
+  if(displayedMeals.length === 0){
+    return (
+      <View style={styles.screen}>
+        <DefaultText>No meals available under this category as per selected Filter. Please check filter...</DefaultText>
+      </View>
+    )
+  }
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
@@ -25,3 +32,12 @@ CategoryMealScreen.navigationOptions = (navigationData) => {
 };
 
 export default CategoryMealScreen;
+
+const styles=StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+  }
+})
